6eb3c361c7fcb349dbdace7bbb3d7aa2b786c2c7
Lucas Torres <lucas@agileana.com>
2019-09-30 08:59:26 -0500

    Add GTM Variable

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,18 @@ module.exports = {
         hostingWPCOM: false,
         useACF: true,
         sourceUrl: 'https://src.lucastorres.co/',
-        replacementUrl: 'https://lucastorres.co/'
+        replacementUrl: 'https://lucastorres.co/',
+        tagManagerId: "GTM-5RD6GCC",
+        manifest: {
+          name: "Lucas Torres",
+          short_name: "Lucas Torres",
+          start_url: "/",
+          background_color: "rgba(0,8,28,0.93)",
+          theme_color: "rgba(255,255,255,0)",
+          display: "standalone",
+          icon: "src/images/icon.png",
+          crossOrigin: `use-credentials`,
+        }
       }
     }
   ]
